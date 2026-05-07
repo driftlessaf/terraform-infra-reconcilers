fa4c383f8624bb0e1d2df3224d3af53a1cb1b563
Evan Gibler <20933572+egibs@users.noreply.github.com>
2026-05-07 12:49:52 -0500

    Export 21e7a2e8a8564e7863df16f5970b07b015ea45b7
    Export:  21e7a2e8a8564e7863df16f5970b07b015ea45b7

--- modules/regional-go-reconciler/variables.tf
+++ modules/regional-go-reconciler/variables.tf
@@ -105,7 +105,8 @@ variable "containers" {
       importpath  = string
       env         = optional(list(string), [])
     })
-    args = optional(list(string), [])
+    command = optional(list(string), [])
+    args    = optional(list(string), [])
     ports = optional(list(object({
       name           = optional(string, "h2c")
       container_port = number
