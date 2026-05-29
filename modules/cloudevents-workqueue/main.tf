246fb12573b54c7382388b6335b2bb961e2672a0
Carlos Tadeu Panato Junior <cpanato@chainguard.dev>
2026-05-29 14:19:45 +0200

    Export 5333dfd516840d0f3cbfd68b54351e8ba32f7bdf
    Export:  5333dfd516840d0f3cbfd68b54351e8ba32f7bdf

--- modules/cloudevents-workqueue/main.tf
+++ modules/cloudevents-workqueue/main.tf
@@ -17,7 +17,7 @@ resource "google_service_account" "subscriber" {
 
 // Deploy the subscriber service
 module "subscriber" {
-  source = "chainguard-dev/common/infra//modules/regional-go-service"
+  source = "../../../../public/terraform-infra-common/modules/regional-go-service"
 
   project_id = var.project_id
   name       = var.name
@@ -54,20 +54,18 @@ module "subscriber" {
       ]
     }
   }
-  version = "1.0.12"
 }
 
 // Authorize the subscriber to call the workqueue in each region
 module "subscriber-calls-workqueue" {
   for_each = var.regions
 
-  source = "chainguard-dev/common/infra//modules/authorize-private-service"
+  source = "../../../../public/terraform-infra-common/modules/authorize-private-service"
 
   project_id      = var.project_id
   region          = each.key
   name            = var.workqueue.name
   service-account = google_service_account.subscriber.email
-  version         = "1.0.12"
 }
 
 // Create a subscription to the broker with filters for the specified event types
@@ -83,7 +81,7 @@ module "trigger" {
     }
   }
 
-  source = "chainguard-dev/common/infra//modules/cloudevent-trigger"
+  source = "../../../../public/terraform-infra-common/modules/cloudevent-trigger"
 
   project_id = var.project_id
   name       = "${var.name}-${each.value.region}-${each.value.index}"
@@ -109,5 +107,4 @@ module "trigger" {
   team = var.team
 
   depends_on = [module.subscriber]
-  version    = "1.0.12"
 }
