8bdc1effd2ca406a6647cc0e2c30e73787b334be
chainguard-mono-export <open-source-publish@chainguard.dev>
2026-07-09 02:13:39 +0000

    Rewrite local references for export

--- modules/cloudevents-workqueue/main.tf
+++ modules/cloudevents-workqueue/main.tf
@@ -17,7 +17,7 @@ resource "google_service_account" "subscriber" {
 
 // Deploy the subscriber service
 module "subscriber" {
-  source = "../../../../public/terraform-infra-common/modules/regional-go-service"
+  source = "chainguard-dev/common/infra//modules/regional-go-service"
 
   project_id = var.project_id
   name       = var.name
@@ -57,18 +57,20 @@ module "subscriber" {
       ]
     }
   }
+  version = "1.13.1"
 }
 
 // Authorize the subscriber to call the workqueue in each region
 module "subscriber-calls-workqueue" {
   for_each = var.regions
 
-  source = "../../../../public/terraform-infra-common/modules/authorize-private-service"
+  source = "chainguard-dev/common/infra//modules/authorize-private-service"
 
   project_id      = var.project_id
   region          = each.key
   name            = var.workqueue.name
   service-account = google_service_account.subscriber.email
+  version         = "1.13.1"
 }
 
 // Create a subscription to the broker with filters for the specified event types
@@ -84,7 +86,7 @@ module "trigger" {
     }
   }
 
-  source = "../../../../public/terraform-infra-common/modules/cloudevent-trigger"
+  source = "chainguard-dev/common/infra//modules/cloudevent-trigger"
 
   project_id = var.project_id
   name       = "${var.name}-${each.value.region}-${each.value.index}"
@@ -111,4 +113,5 @@ module "trigger" {
   team = var.team
 
   depends_on = [module.subscriber]
+  version    = "1.13.1"
 }
