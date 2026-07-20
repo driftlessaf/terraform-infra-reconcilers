cffff6c215be8983ef6b75e4a31ecf5a161661db
chainguard-mono-export <open-source-publish@chainguard.dev>
2026-07-20 15:04:15 +0000

    Rewrite local references for export

--- modules/workqueue/receiver.tf
+++ modules/workqueue/receiver.tf
@@ -21,7 +21,7 @@ resource "google_service_account" "receiver" {
 // Stand up the receiver service in each of our regions.
 module "receiver-service" {
   count              = local.workqueue_enabled ? 1 : 0
-  source             = "../../../../public/terraform-infra-common/modules/regional-go-service"
+  source             = "chainguard-dev/common/infra//modules/regional-go-service"
   observability_role = var.observability_role
   project_id         = local.project_id
   name               = local.receiver_service_name
@@ -80,4 +80,5 @@ module "receiver-service" {
   require_authenticated_invocations = true
 
   notification_channels = local.notification_channels
+  version               = "1.21.1"
 }
