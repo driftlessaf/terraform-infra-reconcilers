31d1ae010f2a30cdae44a4b6bdd711db95b96f51
chainguard-mono-export <open-source-publish@chainguard.dev>
2026-07-30 16:38:49 +0000

    Rewrite local references for export

--- modules/workqueue/reenqueue.tf
+++ modules/workqueue/reenqueue.tf
@@ -33,7 +33,7 @@ resource "google_storage_bucket_iam_member" "reenqueue-bucket-access" {
 // The reenqueue cron job (paused by default, for manual invocation)
 module "reenqueue" {
   count  = local.workqueue_enabled ? 1 : 0
-  source = "../../../../public/terraform-infra-common/modules/cron"
+  source = "chainguard-dev/common/infra//modules/cron"
 
   project_id         = local.project_id
   observability_role = var.observability_role
@@ -77,4 +77,5 @@ module "reenqueue" {
   product               = local.product
   notification_channels = local.notification_channels
   deletion_protection   = local.deletion_protection
+  version               = "1.26.4"
 }
