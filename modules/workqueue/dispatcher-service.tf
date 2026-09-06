a2a067a53b9d821ef648329b6a9b437821b06dcc
chainguard-mono-export <open-source-publish@chainguard.dev>
2026-09-06 14:38:29 +0000

    Rewrite local references for export

--- modules/workqueue/dispatcher-service.tf
+++ modules/workqueue/dispatcher-service.tf
@@ -14,7 +14,7 @@ locals {
 // Not used in long mode, where the dispatcher runs inside a Cloud Run Job.
 module "dispatcher-service" {
   count              = local.dispatcher_service_enabled ? 1 : 0
-  source             = "../../../../public/terraform-infra-common/modules/regional-go-service"
+  source             = "chainguard-dev/common/infra//modules/regional-go-service"
   observability_role = var.observability_role
   project_id         = local.project_id
   name               = local.dispatcher_service_name
@@ -73,4 +73,5 @@ module "dispatcher-service" {
   notification_channels = local.notification_channels
 
   resource_manager_tags = var.resource_manager_tags
+  version               = "1.39.4"
 }
